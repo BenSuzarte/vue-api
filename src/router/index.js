4c295093d02c2273ce69403d7fd48a2a099d5c9e
BenSuzarte <ben.dantassuzarte@gmail.com>
2024-06-03 13:18:14 -0300

    feat: Added employee components (Create | Edit | List) and sweetalert2 imported in main.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    name: 'Create New Employee',
+    component: () => import('../components/create-employee/CreateEmployeeComponent.vue'),
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/list-employees',
+    name: 'List All Emplloyees',
+    component: () => import('../components/list-employee/ListEmployeeComponent.vue'),
+  },
+  {
+    path: '/update-employee/:id',
+    name: 'Update Emplloyee',
+    component: () => import('../components/edit-employee/EditEmployeeComponent.vue'),
   },
 ];
 
